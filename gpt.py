d2e1983862b0c02a96e5da929681cdabcdb42e3b
computerphobe <aryanbharadiyak2006@gmail.com>
2026-05-01 12:43:55 +0530

    hyperparameter tuning

--- gpt.py
+++ gpt.py
@@ -3,16 +3,16 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 64 # how many independent sequences will we process in parallel?
-block_size = 256 # what is the maximum context length for predictions?
-max_iters = 5000
+batch_size = 32 # (ideal: 128)
+block_size = 128 # (ideal: 512)
+max_iters = 5000 # (ideal: 10000)
 eval_interval = 500
 learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 384
-n_head = 6
-n_layer = 6
+n_embd = 192 # embedding dimension (ideal: 768)
+n_head = 3 # number of attention heads (ideal: 12)
+n_layer = 3 # number of transformer layers (ideal: 12)
 dropout = 0.2
 # ------------
 
